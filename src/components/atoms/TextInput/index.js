b7ce5cbf8715c8bb29b8468b131c37223dcbf3b2
doni sanjaya <donis293@gmail.com>
2022-04-08 10:50:47 +0700

    SignIn submit form value

--- src/components/atoms/TextInput/index.js
+++ src/components/atoms/TextInput/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {StyleSheet, Text, View, TextInput as TextInputRN} from 'react-native';
 
-const TextInput = ({label, placeholder}) => {
+const TextInput = ({label, placeholder, ...restProps}) => {
   return (
     <View>
       <Text style={styles.label}>{label}</Text>
-      <TextInputRN style={styles.input} placeholder={placeholder} />
+      <TextInputRN
+        style={styles.input}
+        placeholder={placeholder}
+        {...restProps}
+      />
     </View>
   );
 };
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,15 @@
-import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Button, Gap, Header, TextInput} from '../../components';
 
 const SignIn = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = () => {
+    console.log('emailasas:', email);
+    console.log('password:', password);
+  };
   return (
     <View style={styles.page}>
       <Header title="Sign In" subTitle="Find youre best ever meal" />
@@ -11,11 +17,19 @@ const SignIn = ({navigation}) => {
         <TextInput
           label="Email Address"
           placeholder="Type your email address"
+          value={email}
+          onChangeText={value => setEmail(value)}
         />
         <Gap height={16} />
-        <TextInput label="password" placeholder="Type your password" />
+        <TextInput
+          label="password"
+          placeholder="Type your password"
+          value={password}
+          secureTextEntry
+          onChangeText={value => setPassword(value)}
+        />
         <Gap height={24} />
-        <Button text="Sign In" />
+        <Button text="Sign In" onPress={onSubmit} />
         <Gap height={12} />
         <Button
           text="Create New Account"
